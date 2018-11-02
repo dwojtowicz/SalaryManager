3f6d7961afb59e6cae3911089a6c814c762b115a
zuchwa <wojtowicz.da@gmail.com>
2018-11-02 15:18:58 +0100

    Separate Employee and Salary, create Employee detail

--- src/main/java/com/manage/salary/employee/Employee.java
+++ src/main/java/com/manage/salary/employee/Employee.java
@@ -1,11 +1,12 @@
 package com.manage.salary.employee;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.manage.salary.employee.detail.EmployeeDetail;
+import com.manage.salary.salary.Salary;
 import lombok.Data;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.util.List;
 
 
 @Entity
@@ -16,7 +17,7 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer id;
+    private Long id;
 
     @Column(name = "first_name")
     private String firstName;
@@ -27,17 +28,13 @@ public class Employee {
     @Column(name = "position")
     private String position;
 
-    @Column(name = "gross_month", nullable = false)
-    private Float grossMonth;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "employee_detail_id")
+    private EmployeeDetail employeeDetail;
 
-    @Column(name = "net_month", nullable = false)
-    private Float netMonth;
-
-    @Column(name = "salary_total", nullable = false)
-    private Float salaryTotal;
-
-    @Column(name = "tax", nullable = false)
-    private Integer tax;
+    @OneToMany(mappedBy = "employee",
+    cascade = CascadeType.ALL)
+    private List<Salary> salary;
 
 
 
